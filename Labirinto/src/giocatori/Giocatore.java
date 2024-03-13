fb3878c5f96f9155363c67a39cd5425fd9d884bb
Utente <Utente@DESKTOP-PU5G9A3>
2024-03-13 13:57:48 +0100

    costruttore giocatore

--- Labirinto/src/giocatori/Giocatore.java
+++ Labirinto/src/giocatori/Giocatore.java
@@ -1,5 +1,12 @@
 package giocatori;
 
-public class Giocatore extends Personaggio {
+import risultati.Statistiche;
+import utility.Equip;
+
+public class Giocatore extends Personaggio { 
+
+	public Giocatore(String nome, Statistiche statistiche, Equip equipaggiamento) {
+		super(nome, statistiche, equipaggiamento);
+	}
 
 }
